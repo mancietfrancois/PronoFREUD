faeea2e37d8b2cb46cd9e83be6663512e890c248
David Villard <david0271@ymail.com>
2014-10-08 15:39:42 +0200

    Dépôt projet d'essai sur la synchronisation

---
+++ GrocerySync-Android/SyncTest/src/main/java/com/couchbase/grocerysync/MainActivity.java
@@ -0,0 +1,387 @@
+package com.couchbase.grocerysync;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.couchbase.lite.Database;
+import com.couchbase.lite.Document;
+import com.couchbase.lite.Emitter;
+import com.couchbase.lite.LiveQuery;
+import com.couchbase.lite.Manager;
+import com.couchbase.lite.Mapper;
+import com.couchbase.lite.QueryEnumerator;
+import com.couchbase.lite.QueryRow;
+import com.couchbase.lite.android.AndroidContext;
+import com.couchbase.lite.replicator.Replication;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.UUID;
+
+public class MainActivity extends Activity implements Replication.ChangeListener,
+        OnItemClickListener,  OnItemLongClickListener {
+
+    public static String TAG = "GrocerySync";
+
+    //constants
+    public static final String DATABASE_NAME = "pronos-app";
+    public static final String designDocName = "pronos-local";
+    public static final String byDateViewName = "byDate";
+    //public static final String SYNC_URL = "http://10.0.2.2:4984/grocery-sync";  // 10.0.2.2 == Android Simulator equivalent of 127.0.0.1
+    public static final String SYNC_URL = "http://girafe31.iriscouch.com/pronos-app";  // 10.0.2.2 == Android Simulator equivalent of 127.0.0.1
+    //splash screen
+    protected SplashScreenDialog splashDialog;
+
+    //main screen
+    protected EditText addItemEditText;
+    protected ListView itemListView;
+    protected EventAdapter itemListViewAdapter;
+
+    //couch internals
+    protected static Manager manager;
+    private Database database;
+    private LiveQuery liveQuery;
+
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        //connect items from layout
+
+        itemListView = (ListView)findViewById(R.id.itemListView);
+
+        //connect listeners
+  //      addItemEditText.setOnKeyListener(this);
+
+        //show splash and start couch
+        showSplashScreen();
+        removeSplashScreen();
+
+        try {
+            startCBLite();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Error Initializing CBLIte, see logs for details", Toast.LENGTH_LONG).show();
+            Log.e(TAG, "Error initializing CBLite", e);
+        }
+
+    }
+
+
+    protected void onDestroy() {
+        if(manager != null) {
+            manager.close();
+        }
+        super.onDestroy();
+    }
+
+    protected void startCBLite() throws Exception {
+
+        manager = new Manager(new AndroidContext(this), Manager.DEFAULT_OPTIONS);
+
+        //install a view definition needed by the application
+        database = manager.getDatabase(DATABASE_NAME);
+        com.couchbase.lite.View viewItemsByDate = database.getView(String.format("%s/%s", designDocName, byDateViewName));
+        viewItemsByDate.setMap(new Mapper() {
+            @Override
+            public void map(Map<String, Object> document, Emitter emitter) {
+                Object createdAt = document.get("created_at");
+                if (createdAt != null) {
+                    emitter.emit(createdAt.toString(), null);
+                }
+            }
+        }, "1.0");
+
+
+        startLiveQuery(viewItemsByDate);
+
+        startSync();
+
+    }
+
+    private void startSync() {
+
+        URL syncUrl;
+        try {
+            syncUrl = new URL(SYNC_URL);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+        Replication pullReplication = database.createPullReplication(syncUrl);
+        pullReplication.setContinuous(true);
+
+        Replication pushReplication = database.createPushReplication(syncUrl);
+        pushReplication.setContinuous(true);
+
+        pullReplication.start();
+        pushReplication.start();
+
+        pullReplication.addChangeListener(this);
+        pushReplication.addChangeListener(this);
+
+    }
+
+    private void startLiveQuery(com.couchbase.lite.View view) throws Exception {
+
+        final ProgressDialog progressDialog = showLoadingSpinner();
+
+        if (liveQuery == null) {
+
+            liveQuery = view.createQuery().toLiveQuery();
+
+            liveQuery.addChangeListener(new LiveQuery.ChangeListener() {
+                @Override
+                public void changed(LiveQuery.ChangeEvent event) {
+                    displayRows(event.getRows());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressDialog.dismiss();
+                        }
+                    });
+                }
+            });
+
+            liveQuery.start();
+
+        }
+
+    }
+
+    private void displayRows(QueryEnumerator queryEnumerator) {
+
+        final List<QueryRow> rows = getRowsFromQueryEnumerator(queryEnumerator);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+
+                itemListViewAdapter = new EventAdapter(
+                        getApplicationContext(),
+                        R.layout.event_list_item,
+                        R.id.label,
+                        rows
+                );
+                itemListView.setAdapter(itemListViewAdapter);
+              //  itemListView.setOnItemClickListener(MainActivity.this);
+                itemListView.setOnItemLongClickListener(MainActivity.this);
+
+            }
+        });
+    }
+
+
+    private List<QueryRow> getRowsFromQueryEnumerator(QueryEnumerator queryEnumerator) {
+        List<QueryRow> rows = new ArrayList<QueryRow>();
+        for (Iterator<QueryRow> it = queryEnumerator; it.hasNext();) {
+            QueryRow row = it.next();
+            rows.add(row);
+        }
+        return rows;
+    }
+
+    private ProgressDialog showLoadingSpinner() {
+        ProgressDialog progress = new ProgressDialog(this);
+        progress.setTitle("Loading");
+        progress.setMessage("Wait while loading...");
+        progress.show();
+        return progress;
+    }
+
+
+    /**
+     * Handle typing item text
+     */
+    /*
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        if ((event.getAction() == KeyEvent.ACTION_DOWN)
+                && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+
+            String inputText = addItemEditText.getText().toString();
+            if(!inputText.equals("")) {
+                try {
+
+                    if (inputText.contains(":")) {  // hack to create multiple items
+                        int numCreated = createMultipleGrocerySyncItems(inputText);
+                        String msg = String.format("Created %d new grocery items!", numCreated);
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                    }
+                    else {
+                        createGroceryItem(inputText);
+                        Toast.makeText(getApplicationContext(), "Created new grocery item!", Toast.LENGTH_LONG).show();
+                    }
+
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Error creating document, see logs for details", Toast.LENGTH_LONG).show();
+                    Log.e(TAG, "Error creating document.", e);
+                }
+            }
+            addItemEditText.setText("");
+            return true;
+        }
+        return false;
+    }
+*/
+
+    /**
+     * Handle click on item in list
+     */
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        Log.i(TAG, "click on " + String.valueOf(id));
+        QueryRow row = (QueryRow) adapterView.getItemAtPosition(position);
+        Document document = row.getDocument();
+        Map<String, Object> curProperties = document.getProperties();
+        Map<String, Object> newProperties = new HashMap<String, Object>();
+        newProperties.putAll(curProperties);
+
+
+        Event.Pronostic prono = Event.Pronostic.valueOf(newProperties.get("prono").toString().toUpperCase());
+
+        //newProperties.put("prono", prono.opposite().name().toLowerCase());
+        view.getTag();
+        try {
+          //  document.putProperties(newProperties);
+            itemListViewAdapter.notifyDataSetChanged();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Error updating database, see logs for details", Toast.LENGTH_LONG).show();
+            Log.e(TAG, "Error updating database", e);
+        }
+
+    }
+
+    /**
+     * Handle long-click on item in list
+     */
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+        QueryRow row = (QueryRow) adapterView.getItemAtPosition(position);
+        final Document clickedDocument = row.getDocument();
+        String itemText = (String) clickedDocument.getCurrentRevision().getProperty("text");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        AlertDialog alert = builder.setTitle("Delete Item?")
+                .setMessage("Are you sure you want to delete \"" + itemText + "\"?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        try {
+                            clickedDocument.delete();
+                        } catch (Exception e) {
+                            Toast.makeText(getApplicationContext(), "Error deleting document, see logs for details", Toast.LENGTH_LONG).show();
+                            Log.e(TAG, "Error deleting document", e);
+                        }
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Handle Cancel
+                    }
+                })
+                .create();
+
+        alert.show();
+
+        return true;
+    }
+
+
+    /**
+     * Removes the Dialog that displays the splash screen
+     */
+    protected void removeSplashScreen() {
+        if (splashDialog != null) {
+            splashDialog.dismiss();
+            splashDialog = null;
+        }
+    }
+
+    /**
+     * Shows the splash screen over the full Activity
+     */
+    private void showSplashScreen() {
+        splashDialog = new SplashScreenDialog(this);
+        splashDialog.show();
+    }
+
+    /**
+     * Add settings item to the menu
+     */
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(Menu.NONE, 0, 0, "Settings");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * Launch the settings activity
+     */
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 0:
+                startActivity(new Intent(this, GrocerySyncPreferencesActivity.class));
+                return true;
+        }
+        return false;
+    }
+
+    /*
+
+    private int createMultipleGrocerySyncItems(String text) throws Exception {
+        StringTokenizer st = new StringTokenizer(text, ":");
+        String itemText = st.nextToken();
+        String numItemsString = st.nextToken();
+        int numItems = Integer.parseInt(numItemsString);
+        for (int i = 0; i < numItems; i++) {
+            String curItemText = String.format("%s-%d", itemText, i);
+            createGroceryItem(curItemText);
+        }
+        return numItems;
+    }
+*/
+    @Override
+    public void changed(Replication.ChangeEvent event) {
+
+        Replication replication = event.getSource();
+        Log.d(TAG, "Replication : " + replication + " changed.");
+        if (!replication.isRunning()) {
+            String msg = String.format("Replicator %s not running", replication);
+            Log.d(TAG, msg);
+        }
+        else {
+            int processed = replication.getCompletedChangesCount();
+            int total = replication.getChangesCount();
+            String msg = String.format("Replicator processed %d / %d", processed, total);
+            Log.d(TAG, msg);
+        }
+
+    }
+}
